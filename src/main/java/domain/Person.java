7fa8326d967c69f698c72a0b7b711e35a22a0252
ejdier <s13785@poczta.pjwstk.edu.pl>
2017-05-28 15:25:37 +0200

    test'

---
+++ src/main/java/domain/Person.java
@@ -0,0 +1,48 @@
+package domain;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+@Entity
+public class Person {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String surname;
+	//private List<Car> cars;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+/*	public List<Car> getCars() {
+		return cars;
+	}
+	public void setCars(List<Car> cars) {
+		this.cars = cars;
+	}*/
+	
+}
---
+++ src/main/java/rest/PeopleResources.java
@@ -0,0 +1,106 @@
+package rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import domain.Car;
+import domain.Person;
+import domain.services.PersonService;
+
+@Path("/people")
+@Stateless
+public class PeopleResources {
+
+	private PersonService db = new PersonService();
+	
+	@PersistenceContext
+	EntityManager em;
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Person> getAll() {
+		return db.getAll();
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response Add(Person person) {
+		em.persist(person);
+		return Response.ok(person.getId()).build();
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response get(@PathParam("id") int id){
+		Person result = db.get(id);
+		if(result==null){
+			return Response.status(404).build();
+		}
+		return Response.ok(result).build();
+	}
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response update(@PathParam("id") int id, Person p){
+		Person result = db.get(id);
+		if(result==null){
+			return Response.status(404).build();
+		}
+		p.setId(id);
+		db.update(p);
+		return Response.ok().build();
+	}
+	
+	@DELETE
+	@Path("/{id}")
+	public Response delete(@PathParam("id") int id){
+		Person result = db.get(id);
+		if(result==null){
+			return Response.status(404).build();
+		}
+		db.update(result);
+		return Response.ok().build();
+	}
+	
+/*	@GET
+	@Path("/{personId}/cars")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Car> getCars(@PathParam("personId") int personId){
+		Person result = db.get(personId);
+		if(result==null)
+			return null;
+		if(result.getCars()==null)
+			result.setCars(new ArrayList<Car>());
+		return result.getCars();
+	}
+	
+	@POST
+	@Path("/{id}/cars")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response addCar(@PathParam("id") int personId, Car car){
+		Person result = db.get(personId);
+		if(result==null)
+			return Response.status(404).build();
+		if(result.getCars()==null)
+			result.setCars(new ArrayList<Car>());
+		result.getCars().add(car);
+		return Response.ok().build();
+	}*/
+	
+}
